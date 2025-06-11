15a4095dc077a6a89a2c4b1a2fdef71ac77c6c52
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-06-11 10:00:34 -0300

    feat: create App component with useEffect and main elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
+import { ContainerTeams } from "./components/ContainerTeams/ContainerTeams"
+import { SideBarStacks } from "./components/SideBarStacks/SideBarStacks"
+import axios from "axios"
+import type { teamsType } from "./types/types"
 
 function App() {
+  const [selectedStack, setSelectedStack] = useState<string | null>("Frontend")
+  const [teams, setTeams] = useState<teamsType[]>([])
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const response = await axios.get('/data/teams.json')
+      setTeams(response.data.filter((team: teamsType) => team.team === selectedStack))
+    }
+
+    fetchTeams()
+  }, [selectedStack])
+
   return (
-    <>
-    <h1>Hello!</h1>
-    </>
+    <div className="flex ">
+      <SideBarStacks setSelectedStack={setSelectedStack}/>
+      <ContainerTeams teams={teams}/>
+    </div>
   )
 }
 
